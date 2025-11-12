bc186b8eecd82718363b434e7e552830cf2d18ef
YeziTechie <yazdan.hka@gmail.com>
2025-11-12 13:57:59 +0330

    yacht detail added

--- scripts/detail.js
+++ scripts/detail.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('.yacht-holder').forEach((el) => {
+document.querySelectorAll('.yacht-img').forEach((el) => {
   const delay = Math.random() * 2 + 1; // random number between 1 and 3
   el.style.animationDelay = `${delay}s`;
 
-  const yacht = el.querySelector('.yacht');
+  const yacht = el.querySelector('img');
   if (yacht) {
     const yachtDelay = Math.random() * 2 + 1; // also random 1–3s for inner image
     yacht.style.animationDelay = `${yachtDelay}s`;
